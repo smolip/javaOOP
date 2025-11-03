6e0ddb3037332fed44753a581d0d77b91c9e5f14
smolip <petrsm100@gmail.com>
2025-11-03 16:16:33 +0100

    test

---
+++ test1/src/test1/Test1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package test1;
+import java.util.Scanner;
+/**
+ *
+ * @author smolip06
+ */
+public class Test1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int vstup = sc.nextInt();
+        for (int i=0;i<=vstup;i++) {
+            System.out.println(i);
+        }
+    }
+    
+}
