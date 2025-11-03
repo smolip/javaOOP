d455429c0374908f13a8903c4b72c1daae6e33f2
smolip <petrsm100@gmail.com>
2025-11-03 16:05:54 +0100

    init

---
+++ test/src/main/java/com/mycompany/test/Test.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.test;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author smolip06
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int vstup = sc.nextInt();
+        System.out.println("ahoj svete " + vstup );
+         
+    }
+}
